a0f54ff4d27d52363866facaf9c26966b873c2ea
Manoel Jose Feitosa <manoelbd2012@gmail.com>
2026-05-18 09:16:31 -0300

    Liberação de acesso ao painel Filament para o usuário admin em produção

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
 
@@ -43,5 +45,14 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Determina se o usuário pode acessar o painel do Filament em Produção.
+     */
+    public function canAccessPanel(Panel $panel): bool
+    {
+        // Libera o acesso exclusivamente para o email cadastrado no Seeder
+        return $this->email === 'admin@admin.com';
     }
 }
